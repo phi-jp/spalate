f0155ec505d9c55c84076cb4d96b1eb47b37f71c
phi <phi1618jp@gmail.com>
2018-03-08 16:16:16 +0900

    remove includes

--- test/app/includes.js
+++
@@ -1,6 +0,0 @@
-module.exports = [
-  // assets
-  { type: 'script', path: '/scripts/main.js', },
-  // { type: 'style', path: '/styles/main.css', },
-  // { type: 'style', path: '/fonts/icons/icons.css', },
-];

7d232b34693c18cbd4162c836a7e4bbee926bcfd
phi <phi1618jp@gmail.com>
2018-03-09 17:08:16 +0900

    fix base tags

--- test/public/scripts/tags.js
+++ test/public/scripts/tags.js
@@ -1,7 +1,20 @@
 
-riot.tag2('page-index', '<h1>spalate test222 {10+5}</h1>', '', '', function(opts) {
+riot.tag2('page-index', '<div class="flex flex-column h-full"> <module-header></module-header> <div class="h-full overflow-scroll"> <div class="container p10"> <div class="posts"> <div class="box-shadow p16 rounded-4 bg-white mb20" each="{items}"> <h3 class="text-primary"><a href="/post/{id}">{title}</a></h3> <p>{body}</p> </div> </div> </div> </div> <module-tabbar></module-tabbar> </div>', '', '', function(opts) {
+    var self = this;
+
+    this.on('show', function() {
+      app.ref.child('items').get().then((res) => {
+        this.items = res.data.items;
+        this.update();
+      });
+    });
+
+    this.on('fetch', function(e) {
+      this.posts = e.response;
+      this.update();
+    });
 });
 
 
-riot.tag2('atom-input', '<input>', '', '', function(opts) {
+riot.tag2('page-settings', '<h2>settings</h2>', '', '', function(opts) {
 });

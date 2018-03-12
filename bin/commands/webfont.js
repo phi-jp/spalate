bf84757881f58051c3873345f427de1e06a8aae2
phi <phi1618jp@gmail.com>
2018-03-13 01:10:06 +0900

    fix webfont task

--- bin/commands/webfont.js
+++ bin/commands/webfont.js
@@ -1,37 +1,24 @@
-var fs      = require('fs-extra');
 var path    = require('path');
-var webfont = require('webfont-ex').default
-var config  = require('config');
+var config  = require('config').spalate.webfont;
+var exec    = require('child_process').exec
 
-// node-glob
+var target = path.join(process.cwd(), config.target);
+var output = path.join(process.cwd(), config.output);
+var cmd = 'fontcustom compile';
 
-var target = path.join(process.cwd(), config.spalate.webfont.target);
-var output = path.join(process.cwd(), config.spalate.webfont.output);
+config.options = config.options || {};
+config.options['font_name'] = config.options['font_name'] || path.basename(output);
+config.options['output'] = config.output;
 
-async function run() {
-  await fs.remove(output)
-  await fs.mkdirs(output + '/_temp');
-  await fs.copy(target, output + '/_temp');
-  
-  webfont({
-    files: output + '/_temp/*.svg',
-    fontName: 'iconfont',
-    prependUnicode: true,
-    startUnicode: 0xF001,
-    template: "css",
-    cssTemplateClassName: 'icon',
-    cssTemplateFontName: 'iconfont',
-    normalize: true,
-    fontHeight: 512,
-  }).then((result) => {
-    fs.writeFileSync(output + '/iconfont.ttf', result.ttf )
-    fs.writeFileSync(output + '/iconfont.woff', result.woff )
-    fs.writeFileSync(output + '/iconfont.woff2', result.woff2 )
-    fs.writeFileSync(output + '/iconfont.css', result.styles)
-    console.log(result);
-  });  
-};
+var args = [];
+for (var key in config.options) {
+  args.push('--' + key, config.options[key]);
+}
 
-run();
+var opts = args.join(' ');
+
+exec([cmd, target, opts].join(' '), (error, stdout, stderr) => {
+  console.log(stdout);
+});
 
 

c226f6a8572321dfa2f5f77bb21d168cea34bfa4
simiraaaa <c7t2h8m3y0@yahoo.co.jp>
2018-03-13 15:35:10 +0900

    refactor dev

--- bin/commands/dev.js
+++ bin/commands/dev.js
@@ -1,8 +1,4 @@
-var fs        = require('fs');
-var path      = require('path');
-var chokidar  = require('chokidar');
-var config    = require('config');
-var riot      = require('riot');
+var builder = require('./lib/riot-builder');
 
 var Spalate = require('../..');
 var app = Spalate();
@@ -16,30 +12,6 @@ var server = app.listen(app.get('port'), function() {
   console.log('Example app listening at http://%s:%s', host, port);
 });
 
-
-// watch and build
-var watcher = chokidar.watch(config.spalate.riot.target, {
-  ignored: /[\/\\]\./,
-  persistent: true,
-});
-
-var files = {};
-
-watcher.on('all', (event, file) => {
-  if (/^change$|^add$/.test(event)) {
-    var code = fs.readFileSync(file, 'utf8').toString();
-
-    var js = riot.compile(code, config.spalate.riot.options);
-
-    files[file] = js;
-  }
-  if (/^unlink$/.test(event)) {
-    delete files[file];
-  }
-  fs.writeFileSync(path.join(config.spalate.riot.output, 'tags.js'), Object.keys(files).map(file => files[file]).join('\n\n'));
-  console.log('output file');
-});
-
-watcher
+builder.watch().watcher
   .on('ready', function() { console.log("監視開始"); })
   .on('change', function(path) { console.log("修正されました-> " + path); })

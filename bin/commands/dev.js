6a752939306c73e5681c957bbd16448519f84719
simiraaaa <c7t2h8m3y0@yahoo.co.jp>
2018-04-19 16:10:37 +0900

    fix server restart if changed router.js and config

--- bin/commands/dev.js
+++ bin/commands/dev.js
@@ -1,15 +1,46 @@
 var builder = require('./lib/riot-builder');
+var chokidar = require('chokidar');
+var colors = require('colors');
+var path = require('path');
+var config = require('config');
 
-var Spalate = require('../..');
-var app = Spalate();
+var server;
+var serverPromise = Promise.resolve();
 
-app.set('port', process.env.PORT || 3000);
+var startServer = () => {
+  return serverPromise = serverPromise.then(new Promise(resolve => {
+    var Spalate = require('../..');
+    var app = Spalate();
+    app.set('port', process.env.PORT || 3000);
+    // launch server
+    server = app.listen(app.get('port'), function() {
+      var host = server.address().address;
+      var port = server.address().port;
+      console.log('Example app listening at http://%s:%s', host, port);
+      resolve();
+    });
+  }));
+};
 
-// launch server
-var server = app.listen(app.get('port'), function() {
-  var host = server.address().address;
-  var port = server.address().port;
-  console.log('Example app listening at http://%s:%s', host, port);
+var restartServer = () => {
+  // config や router の定義を再読込させるため、すべての require のキャッシュを削除する
+  for (var k in require.cache) {
+    delete require.cache[k];
+  }
+  return serverPromise.then(new Promise(resolve => {
+    console.log('Server Restart'.cyan);
+    server.close(resolve);
+  })).then(startServer);
+  return serverPromise;
+};
+
+startServer();
+var routerPath = path.resolve(path.join(process.cwd(), config.spalate.router));
+chokidar.watch([routerPath, path.join('config')], {
+  ignored: /[\/\\]\./,
+  persistent: true,
+}).on('change', function(path) {;
+  restartServer();
 });
 
 const watchingBuilder = builder.watch();

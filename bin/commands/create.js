97ddf581f4f5e9a0ca42da77a4f836214ace2a7f
simiraaaa <c7t2h8m3y0@yahoo.co.jp>
2018-06-04 16:38:39 +0900

    fix gitignore

--- bin/commands/create.js
+++ bin/commands/create.js
@@ -102,7 +102,7 @@ new Promise(resolve => {
   });
 }).then(function(res) {
   var files = [
-    '.gitignore',
+    'gitignore',
   ];
   var dirs = [
     'app',
@@ -112,11 +112,11 @@ new Promise(resolve => {
   ];
 
   files.forEach(file => {
-    var src = file;
-    if (file === '.gitignore') {
-      src = 'gitignore';
+    var dist = file;
+    if (dist === 'gitignore') {
+      dist = '.gitignore';
     }
-    fs.copyFileSync(path.join(templatePath, src), path.join(distDir, file));
+    fs.copyFileSync(path.join(templatePath, file), path.join(distDir, dist));
   });
 
   dirs.forEach(dir => {

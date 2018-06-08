f23a9732e51593bf266c51e3ae4836c8deb57f2b
simiraaaa <c7t2h8m3y0@yahoo.co.jp>
2018-06-08 18:28:44 +0900

    version 表示と help 対応

--- bin/spalate.js
+++ bin/spalate.js
@@ -3,15 +3,61 @@
 var path      = require('path');
 var program   = require('commander');
 var version = require(path.join(__dirname, '..', 'package.json')).version;
-program
-  .version(version, '-v, --version')
-  .parse(process.argv);
+
+var commands = {
+  start: {
+    description: 'サーバーを起動します',
+  },
+  build: {
+    description: 'ビルドします',
+  },
+  cordova: {
+    description: 'cordova のセットアップをします',
+  },
+  webfont: {
+    description: 'webfont を作成します',
+  },
+  dev: {
+    description: 'サーバーを起動し、ビルドの監視を行います',
+  },
+  deploy: {
+    description: 'デプロイを行います',
+    args: '[type(circleci)]',
+  },
+  create: {
+    description: 'spalate のテンプレート環境を作成します。\n\tdirname を省略すると現在のディレクトリを指定したことになります',
+    args: '[dirname]',
+  },
+  generate: {
+    description: 'テンプレートからファイルを作成します',
+    args: '[type(tag)] [output]',
+  },
+};
+
+for (let key in commands) {
+  let command = commands[key];
+  let description = '';
+  if (command.args) {
+    description = command.args + '\t' + command.description;
+  }
+  else {
+    description = command.description;
+  }
+  // 一旦説明文を -h で表示できるようにするだけ
+  program.command(`${key}`, description);
+}
 
 var defaultCommand = 'dev';
 var cmd = process.argv[2];
 
-if ([defaultCommand, 'build', 'start', 'webfont', 'deploy', 'generate', 'create', 'cordova'].indexOf(cmd) === -1) {
+if (cmd === '') {
   cmd = defaultCommand;
 }
 
-require( path.join(__dirname, 'commands', cmd) );
+if (Object.keys(commands).indexOf(cmd) !== -1) {
+  require(path.join(__dirname, 'commands', cmd));
+}
+else {
+  program.version(version, '-v, --version');
+  program.parse(process.argv);
+}

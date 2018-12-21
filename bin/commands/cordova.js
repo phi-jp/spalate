3e315bf508befbe28f6b0e34150aad1271fcff47
naka <snuffy.tokyo@gmail.com>
2018-12-22 00:58:42 +0900

    fix error message

--- bin/commands/cordova.js
+++ bin/commands/cordova.js
@@ -9,10 +9,16 @@ const pathToCofigXml = config.target + '/config.xml';
 let appVersion = '';
 
 (async () => {
+  // config.xml があるかどうかチェック
+  let exists = await fse.pathExists(pathToCofigXml);
+  if (!exists) {
+    console.log(' ✗  Error: '.red + "Cannot find config.xml. Please check below directory \n" + "　→　" + pathToCofigXml);
+    return;
+  }
 
   // xml を パース
   const xmlResult = await readFileFromConfigXml(pathToCofigXml).catch(err => {
-    console.log("Err! Could't read or parse file from config xml \n".red + err);
+    console.log(" ✗  Error: ".red + "Cannot read or parse file from config xml \n" + err);
     return;
   });
 
@@ -54,26 +60,29 @@ function readFileFromConfigXml(path) {
 async function copyToPlatformFolder(platform) {
 
   // コピースタートを宣言
-  console.log( 'Copying ' + platform + ' ...')
+  console.log( '    Copying ' + platform + ' ...')
 
   // Config ファイルに input output 先が明記されているか
   if (!config.target || !config.output)  {
-    return console.error("Error".red + ": could't find the path target or output");
-  }
-  var targetPath = config.target + '/platforms/' + platform + '/platform_www';
-
-  // Cordova プロジェクト配下に プラットフォームがあるかどうか
-  let exists = await fse.pathExists(targetPath);
-  if (!exists) {
-    return console.log(' ✗  '.red + "Cannot find " + platform + " folder on app directry. Please checking directory")
+    return console.error("Error: ".red + ": could't find the path target or output");
   }
   
+  let targetPlatform = config.target + '/platforms/' + platform;
+
+  // Cordova プロジェクト配下に プラットフォームがあるかどうか
+  let exists = await fse.pathExists(targetPlatform);
+  if (!exists) {
+    return console.log(' ✗  '.red + "Cannot find " + platform + " folder on application directry. Please checking application directory\n" + "　→　" + targetPlatform);
+  }
+  
+  // コピー先とコピー元を指定
+  let targetPath = targetPlatform + '/platform_www/';
   let output = config.output + '/' + platform + '/' + appVersion;
 
   // Copy 実行
   try {
-    await fse.copy(targetPath, output);
-    console.log(' ✓  '.green + platform +": Success copying!");
+    await fse.copy(targetPath , output);
+    console.log(' ✓  '.green + 'Success copying!: ' + platform);
   }
   catch(err) {
     console.error(' ✗  '.red + err);
